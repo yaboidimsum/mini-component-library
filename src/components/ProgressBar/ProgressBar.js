fd80214571400c618c7672034f951371f3b0176f
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2025-02-12 18:37:58 +0700

    feat: progressbar & select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,61 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--padding": "0px",
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": "0px",
+  },
+  large: {
+    "--height": "20px",
+    "--padding": "4px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const Styles = SIZES[size];
+
+  return (
+    <ProgressWrapper style={Styles}>
+      <VisuallyHidden>Progress Bar</VisuallyHidden>
+      <ProgressBase value={value} max={100} />
+    </ProgressWrapper>
+  );
 };
+
+const ProgressWrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  position: relative;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressBase = styled.progress`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  position: absolute;
+  padding: var(--padding);
+
+  &::-webkit-progress-bar {
+    background: transparent;
+  }
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: ${(props) =>
+      props.value === 100.0 ? "4px" : "4px 0px 0px 4px"};
+  }
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useId } from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectComponent value={value} onChange={onChange}>
+        {children}
+      </SelectComponent>
+      <DisplaySelect>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id={`chevron-down`} size={24} strokeWidth={2} />
+        </IconWrapper>
+      </DisplaySelect>
+    </Wrapper>
   );
 };
+
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const SelectComponent = styled.select`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const DisplaySelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  padding-right: 52px;
+  appearance: none;
+  border-radius: 8px;
+  border: none;
+  width: 100%;
+
+  ${SelectComponent}:focus + & {
+    outline: 2px solid hsla(218, 57%, 53%, 1);
+  }
+
+  ${SelectComponent}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  pointer-events: none;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+`;
 
 export default Select;
